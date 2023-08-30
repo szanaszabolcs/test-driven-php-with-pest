ed68eae266fd9d65d6011345e521bc5af76c8afc
Szabolcs Szana <szana.szabolcs@gmail.com>
2023-08-30 22:02:46 +0200

    Feature Tests - Create an API Test

---
+++ tests/Feature/BooksApiTest.php
@@ -0,0 +1,21 @@
+<?php
+
+it('retrieves the correct data from the books API', function () {
+    // ARRANGE
+
+    // ACT
+    $response = $this->json(method: 'GET', uri: '/api/books/1');
+
+    // ASSERT
+    expect($response->getStatusCode())->toBeInt()->toBe(200)
+        ->and($response->getBody())->toMatchJson([
+            'id' => 1,
+            'title' => 'Clean Code: A Handbook of Agile Software Craftsmanship',
+            'year_published' => 2008,
+            'author' => [
+                'id' => 1,
+                'name' => 'Robert C. Martin',
+                'bio' => 'This is an author',
+            ],
+        ]);
+});
